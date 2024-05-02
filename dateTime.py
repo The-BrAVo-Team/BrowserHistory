12533bb5abc7106b3452bc764845444f3fca9062
andrew-g-may000 <147677469+andrew-g-may000@users.noreply.github.com>
2024-05-02 13:11:19 -0500

    Add files via upload

---
+++ dateTime.py
@@ -0,0 +1,11 @@
+import pandas as pd
+from datetime import datetime
+
+import datetime
+
+start = datetime.datetime.strptime("21-06-2020", "%d-%m-%Y")
+end = datetime.datetime.strptime("07-07-2021", "%d-%m-%Y")
+date_generated = [start + datetime.timedelta(days=x) for x in range(0, (end-start).days)]
+
+for date in date_generated:
+    print(date.strftime("%d-%m-%Y"))
