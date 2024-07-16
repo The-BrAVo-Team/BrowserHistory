7fc28b510d4e94a708bfd598170f13773a43dad0
Bethvour <bethvourc@gmail.com>
2024-07-15 22:17:24 -0700

    UI update

--- accessHistory.py
+++ accessHistory.py
@@ -1,8 +1,21 @@
 import sqlite3
 
-con =sqlite3.connect("C:\\Users\\keoca\\Desktop\\TWP3\\TestUser\\Default\\History")
+def fetch_history(db_path="C:\\Users\\keoca\\Desktop\\TWP3\\TestUser\\Default\\History"):
+    """
+    Fetches browsing history from the SQLite database.
+    """
+    try:
+        con = sqlite3.connect(db_path)
+        cur = con.cursor()
+        res = cur.execute("SELECT url FROM urls ORDER BY last_visit_time")
+        history = res.fetchall()
+        con.close()
+        return [url[0] for url in history]  # Return a list of URLs
+    except sqlite3.Error as e:
+        print(f"An error occurred: {e}")
+        return []
 
-cur = con.cursor()
-
-res = cur.execute("SELECT url FROM urls ORDER BY last_visit_time")
-print(res.fetchall())
+if __name__ == '__main__':
+    history = fetch_history()
+    for url in history:
+        print(url)
--- googleAutomate.py
+++ googleAutomate.py
@@ -4,48 +4,64 @@ from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
 from webcrawler import web_crawler
 import random as rnd
+import os, shutil;
 
-def read_keywords_from_file(file_path):
-    with open(file_path, 'r') as file:
-        keywords = [line.strip() for line in file.readlines()]
-    return keywords
-
-chrome_options = webdriver.ChromeOptions()
-# Path to any folder where webdriver chrome data will go
-path = "C:\\Users\\keoca\\Desktop\\TWP3\\TestUser"
-chrome_options.add_argument("--user-data-dir=" + path)
-# Name of user folder which will conatin History file
-chrome_options.add_argument("--profile-directory=Default")
-service = ChromeService(ChromeDriverManager().install())
-driver = webdriver.Chrome(service=service, options=chrome_options)
-
-search_engines = read_keywords_from_file("search_engines.txt")
-
-query_list = read_keywords_from_file("keywords.txt")
-
-links = []
-
-rnd.shuffle(query_list)
-
-for query in query_list:
-    query = query.replace(' ', '+')  
-    # Specify number of pages on google search, each page contains 10 links
-    n_pages = 3
-    for page in range(1, n_pages):
-        url = rnd.choice(search_engines) + query + "&start=" + str(page)
-        links.append(url)
-        driver.get(url)
-        soup = BeautifulSoup(driver.page_source, 'html.parser')
-
-        search = soup.find_all('div', class_="yuRUbf")
-        for h in search:
-            links.append(h.a.get('href'))
-        links.extend(web_crawler(rnd.choice(links)))
+class GoogleAutomation:
+    def __init__(self, path, keywordList, searchEngineList, overwrite=False, ):
+        self.path = path
+        self.overwrite = overwrite
+        self.keywordList = self.read_keywords_from_file(keywordList)
+        self.searchEngineList = self.read_keywords_from_file(searchEngineList)
+        self.countLinks = 0
+        self.links = []
         
-links = set(links)
-driver.quit()
-            
+    
+    def read_keywords_from_file(self, file_path):
+        with open(file_path, 'r') as file:
+            keywords = [line.strip() for line in file.readlines()]
+        return keywords
+    
+    def run(self):
+        chrome_options = webdriver.ChromeOptions()
+        # Path to any folder where webdriver chrome data will go
+        # WILL OVERWRITE PATH/Default
+        # Removes the Default folder which contains the history files to start from a clean slate.
+        if os.path.exists(self.path + "\\Default"):
+            shutil.rmtree(self.path + "\\Default")
+        chrome_options.add_argument("--user-data-dir=" + self.path)
+        # Name of user folder which will conatin History file
+
+        chrome_options.add_argument("--profile-directory=Default")
+        service = ChromeService(ChromeDriverManager().install())
+        driver = webdriver.Chrome(service=service, options=chrome_options)
+
+        rnd.shuffle(self.keywordList)
+
+        for query in self.keywordList:
+            query = query.replace(' ', '+')  
+            # Specify number of pages on google search, each page contains 10 links
+            n_pages = 3
+            for page in range(1, n_pages):
+                url = rnd.choice(self.searchEngineList) + query + "&start=" + str(page)
+                self.links.append(url)
+                driver.get(url)
+                soup = BeautifulSoup(driver.page_source, 'html.parser')
+        """
+                Doing Extra work that does not get added to the History File. May want to rework this.
+                search = soup.find_all('div', class_="yuRUbf")
+                for h in search:
+                self.links.append(h.a.get('href'))
+                self.links.extend(web_crawler(rnd.choice(self.links)))
+        
+        """
+                
+                
+        links = set(self.links)
+        print(links, len(links))
+        driver.quit()
+
 
 if __name__ == '__main__':      
-    print(links)
+    ga = GoogleAutomation( "C:\\Users\\keoca\\Desktop\\TWP3\\TestUser", "keywords.txt", "search_engines.txt")
+    ga.run()
     quit()
