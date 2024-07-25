86c05d00f5f08baedc846da7fc0674a95046a762
Vincent Carlton <vcarlton@nevada.unr.edu>
2024-07-24 20:38:29 -0700

    Changed the way webcrawler works

--- googleAutomate.py
+++ googleAutomate.py
@@ -29,8 +29,15 @@ class GoogleAutomation:
     def read_keywords_from_file(self, file_path):
         with open(file_path, 'r') as file:
             keywords = [line.strip().split() for line in file.readlines()]
-            print(keywords)
         return keywords
+    
+    def visit(self,driver, url):
+        try:
+            print("Visiting: " + url)
+            driver.get(url)
+        except:
+            print("Failed to get page " + url)
+        
     
     def run(self):
 
@@ -43,26 +50,26 @@ class GoogleAutomation:
         # Removes the Default folder which contains the history files to start from a clean slate.
         if os.path.exists(self.path + "\\Default") and self.overwrite:
             shutil.rmtree(self.path + "\\Default")  
-        #gChromeOptions.add_argument("--headless=new") #breaks duckduckgo searches but speeds up google searches
+        gChromeOptions.add_argument("--headless=new") #breaks duckduckgo searches but speeds up google searches
         gChromeOptions.add_argument("--disable-gpu")
         gChromeOptions.add_argument("--no-sandbox")
         gChromeOptions.add_argument("--disable-dev-shm-usage")
         gChromeOptions.add_argument("--user-data-dir=" + self.path)
         gChromeOptions.add_argument("--profile-directory=Default")
         gChromeOptions.add_argument("--mute-audio")
-        gChromeOptions.page_load_strategy = 'none'
-        gChromeOptions.add_experimental_option("prefs", {
-            #block image loading
-            "profile.managed_default_content_settings.images": 2,
-        })
+        gChromeOptions.page_load_strategy = 'eager'
+        # gChromeOptions.add_experimental_option("prefs", {
+        #     #block image loading
+        #     "profile.managed_default_content_settings.images": 2,
+        # })
         service = ChromeService(ChromeDriverManager().install())
         googleDriver = webdriver.Chrome(service=service, options=gChromeOptions)
-        googleDriver.implicitly_wait(.2)
         
         rnd.shuffle(self.keywordList)
+        links = []
+        #while < 1000 links 
         for query in self.keywordList:
             query = "+".join(query)
-            print(query, type(query))
             # Specify number of pages on google search, each page contains 10 links
             n_pages = 3
             
@@ -83,18 +90,20 @@ class GoogleAutomation:
                 soup = BeautifulSoup(googleDriver.page_source, 'html.parser')
                 search = soup.find_all('div', class_=divClass)
                 print(str(len(search)) + " links found")
-                if search:
-                    for h in search:
-                        print("Visiting page " + str(h.a.get('href')))
-                        googleDriver.get(h.a.get('href'))
-                        self.count += 1
-                        try:
-                            WebDriverWait(googleDriver, timeout=.2).until(
-                                ec.visibility_of_element_located((By.TAG_NAME, "html"))
-                            )
-                        except:
-                            print("timeout while loading page")
-        googleDriver.quit()        
+                links.extend([h.a.get('href') for h in search])
+        googleDriver.quit() 
+        gChromeOptions.add_experimental_option("prefs", {
+            #block image loading
+            "profile.managed_default_content_settings.images": 2,
+            "profile.managed_default_content_settings.javascript": 2,
+        })
+        webDriver = webdriver.Chrome(service=service, options=gChromeOptions)
+        webDriver.set_page_load_timeout(5)
+        if links:
+            for url in links:
+                self.visit(webDriver,url)
+                self.count += 1
+        webDriver.quit()
         print(self.count)
 
 
