4e4f46657b6f690b218d11c3d5276a6f0e2e4000
Vincent Carlton <vcarlton@nevada.unr.edu>
2024-08-14 11:50:54 -0700

    Reworked the way the googleAutomate works and added answer key.

--- googleAutomate.py
+++ googleAutomate.py
@@ -16,10 +16,7 @@ start_time = time.time()
 class GoogleAutomation:
     def __init__(self, scenario, overwrite=False, ):
         # Name of user folder which will conatin History file
-        dirname = os.path.dirname(__file__)
         username = os.getlogin()
-        
-        #f'C:\\Users\\{username}\\Desktop\\ChromeData'
         
         self.path = f'C:\\Users\\{username}\\AppData\\Local\\Google\\Chrome\\User Data'
         
@@ -37,8 +34,10 @@ class GoogleAutomation:
         # Overwrite T/F
         self.overwrite = overwrite
         # List of Keywords
-        self.keywordList = self.read_keywords_from_file("keywords.txt") + self.read_keywords_from_file(scenario_keywords[scenario])
-        print(self.keywordList)
+        self.keywordList = self.read_keywords_from_file("keywords.txt")
+        self.bad_keywords = self.read_keywords_from_file(scenario_keywords[scenario])
+        
+        print(self.keywordList, self.bad_keywords)
         # List of Search Engines
         self.searchEngineList = self.read_keywords_from_file("search_engines.txt")
         self.count = 0
@@ -78,45 +77,7 @@ class GoogleAutomation:
         gChromeOptions.add_argument("--disable-dev-shm-usage")
         gChromeOptions.add_argument("--mute-audio")
         gChromeOptions.page_load_strategy = 'eager'
-        googleDriver = webdriver.Chrome( options=gChromeOptions)
         
-        rnd.shuffle(self.keywordList)
-        links = []
-        
-        detected = False
-        unsearched_links = []
-        #while < 1000 links 
-        for query in self.keywordList:
-            query = "+".join(query)
-            # Specify number of pages on google search, each page contains 10 links
-            n_pages = 3
-            
-            for page in range(1, n_pages):
-                
-                url, divClass = rnd.choice(self.searchEngineList)#Choses a random search engine from the list of engines
-                url = url + query + "&start=" + str(page)
-                if not detected:
-                    print("Visiting page " + str(url))
-                    googleDriver.get(url)
-                    
-                    try:
-                        WebDriverWait(googleDriver, timeout=10).until(
-                            ec.visibility_of_element_located((By.CLASS_NAME, divClass))
-                        )
-                        links.append(url)
-                        soup = BeautifulSoup(googleDriver.page_source, 'html.parser')
-                        search = soup.find_all('div', class_=divClass)
-                        print(str(len(search)) + " links found")
-                        links.extend([h.a.get('href') for h in search])
-                    except:
-                        print("Search Engine timeout")
-                        detected = True
-                        unsearched_links.append(url)
-                else:
-                    print("search skipped")
-                    unsearched_links.append(url)
-                
-        googleDriver.quit() 
         
         webOptions = webdriver.ChromeOptions()  
         webOptions.add_argument("--headless=new") #breaks duckduckgo searches but speeds up google searches
@@ -130,48 +91,98 @@ class GoogleAutomation:
         webOptions.add_experimental_option("prefs", {
             "profile.managed_default_content_settings.javascript": 2,
         })
-        webDriver = webdriver.Chrome( options=webOptions)
-        webDriver.set_page_load_timeout(5)
-        if links:
-            for url in links:
-                self.visit(webDriver,url)
-                self.count += 1
-        webDriver.quit()
-        
-        googleDriver = webdriver.Chrome( options=gChromeOptions)
-        links2 =[]
         
         
         
-        for url in unsearched_links:
-            googleDriver.get(url)
-            print("Visiting page " + str(url))
-            try:
-                WebDriverWait(googleDriver, timeout=10).until(
-                    ec.visibility_of_element_located((By.CLASS_NAME, divClass))
-                )
-                links2.append(url)
-                soup = BeautifulSoup(googleDriver.page_source, 'html.parser')
-                search = soup.find_all('div', class_=divClass)
-                print(str(len(search)) + " links found")
-                links2.extend([h.a.get('href') for h in search])
-            except:
-                print("Search Engine timeout")
+        rnd.shuffle(self.keywordList)
+        links = []
+        answerKey = []
+        detected = False
         
-        webDriver = webdriver.Chrome( options=webOptions)
-        webDriver.set_page_load_timeout(5)
-        if links2:
-            for url in links2:
-                self.visit(webDriver,url)
-                self.count += 1
-        webDriver.quit()
-        
+        while self.count < 1000:
+            n_pages = 2
+            googleDriver = webdriver.Chrome( options=gChromeOptions)
+            while not detected:
+                
+                if rnd.randint(0,10) == 10 and len(self.bad_keywords) > 3:
+                    #Suspicious search
+                    query = rnd.choice(self.bad_keywords)
+                    query = "+".join(query)
+                    for page in range(1, n_pages+1):
+                    
+                        url, divClass = rnd.choice(self.searchEngineList)#Choses a random search engine from the list of engines
+                        url = url + query + "&start=" + str(page)
+                        print("Visiting page " + str(url))
+                        try:
+                            googleDriver.get(url)
+                            
+                            WebDriverWait(googleDriver, timeout=10).until(
+                                ec.visibility_of_element_located((By.CLASS_NAME, divClass))
+                            )
+                            links.append(url)
+                            answerKey.append(url)
+                            soup = BeautifulSoup(googleDriver.page_source, 'html.parser')
+                            search = soup.find_all('div', class_=divClass)
+                            print(str(len(search)) + " links found")
+                            links.extend([h.a.get('href') for h in search])
+                            answerKey.extend([h.a.get('href') for h in search])
+                        except:
+                            print("Search Engine timeout")
+                            detected = True
+                            break
+                    
+                    
+                else:
+                    query = rnd.choice(self.keywordList)
+                    query = "+".join(query)
+                    for page in range(1, n_pages+1):
+                    
+                        url, divClass = rnd.choice(self.searchEngineList)#Choses a random search engine from the list of engines
+                        url = url + query + "&start=" + str(page)
+                        print("Visiting page " + str(url))
+                        googleDriver.get(url)
+                        
+                        try:
+                            WebDriverWait(googleDriver, timeout=10).until(
+                                ec.visibility_of_element_located((By.CLASS_NAME, divClass))
+                            )
+                            links.append(url)
+                            soup = BeautifulSoup(googleDriver.page_source, 'html.parser')
+                            search = soup.find_all('div', class_=divClass)
+                            print(str(len(search)) + " links found")
+                            links.extend([h.a.get('href') for h in search])
+                        except:
+                            print("Search Engine timeout")
+                            detected = True
+                            break
+                    
+            googleDriver.quit()
+            #Go through all of the links
+            webDriver = webdriver.Chrome( options=webOptions)
+            webDriver.set_page_load_timeout(5)
+            while links and self.count < 1000:
+                url = links.pop(0)
+                try:
+                    self.visit(webDriver,url)
+                    self.count += 1
+                except:
+                    print("failed, unknown error")
+            for handle in webDriver.window_handles:
+                webDriver.switch_to.window(handle)
+                webDriver.close()
+            webDriver.quit()
+            detected = False
+            
         webOptions.add_experimental_option("prefs", {
             "profile.managed_default_content_settings.javascript": 1,
         })
         webDriver = webdriver.Chrome( options=webOptions)
         webDriver.quit()
         print(self.count)
+        username = os.getlogin()
+        answerfile = open(f'C:\\Users\\{username}\\Desktop\\Answer Key.txt','w')
+        answerfile.writelines(line + '\n' for line in answerKey)
+        answerfile.close()
         modify_timestamps("2024-1-1", "2024-12-31", "20:00:00", "23:59:59")
         
         
@@ -182,7 +193,7 @@ class GoogleAutomation:
 
 
 if __name__ == '__main__':      
-    ga = GoogleAutomation( "clean", overwrite=True)
+    ga = GoogleAutomation( "ip", overwrite=True)
     ga.run()
     print()
     print("--- %s seconds ---" % (time.time() - start_time))
